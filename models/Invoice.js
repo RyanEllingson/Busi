32f178aa7272a64ddac7287efb6dbded9f2eff5e
ivers523 <ivers523@umn.edu>
2020-01-07 13:59:59 -0600

    sequelize models

---
+++ models/Invoice.js
@@ -0,0 +1,22 @@
+module.exports = function(sequelize, DataTypes) {
+    const Invoice = sequelize.define("Invoice", {
+        salesorder_id: {
+            type:DataTypes.INT,
+            allowNull: false
+          },
+          amount_paid: {
+            type:DataTypes.DECIMAL(10,2),
+          },
+          discount: {
+            type:DataTypes.DECIMAL(10,2),
+          },
+          amount_remaining: {
+            type:DataTypes.DECIMAL(10,2),
+          },
+          paid: {
+            type:DataTypes.BOOLEAN,
+            defaultValue: false,
+          }
+    });
+    return Invoice;
+  };
